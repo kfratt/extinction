df965bc11c5a77d18a4780bb4e40b625d29f2320
Rachel Hamre <rh154180@umconnect.umt.edu>
2024-02-17 10:46:03 -0700

    some graphs

--- doi_data_cleaning.R
+++ doi_data_cleaning.R
@@ -8,7 +8,7 @@ library(tidyverse)
 # remove x's for n/as, and removed sessions for June 7 onwards.
 # June 8-16 training was focused on generalizing the DOI, not actual extinction.
 
-#setwd("C:/Documents/K9C/DOI Paper")
+setwd("C:/Documents/K9C/DOI Paper")
 doi_clean <- read.csv("doi_data_clean.csv")
 library(tidyverse)
 library(dplyr)
@@ -25,7 +25,6 @@ doi_clean$X..of.misses <- as.numeric(doi_clean$X..of.misses)
 
 
 # split into two dataframes based on dog learner
-library(dplyr)
 madi <- filter(doi_clean, Dog == "Madi")
 persi <- filter(doi_clean, Dog == "Persi")
 
@@ -47,29 +46,45 @@ persi <- filter(persi, Negative.1 != "none")
 madi <- filter(madi, Cheetah != "none")
 persi <- filter(persi, Cheetah != "none")
 
+#Add column FAduration as sum of false alerts
+madi <- madi %>%
+  mutate(FAduration = rowSums(select(., FA1..s.:FA8..s.), na.rm = TRUE))
+persi <- persi %>%
+  mutate(FAduration = rowSums(select(., FA1..s.:FA8..s.), na.rm = TRUE))
+
+
 
 ######################################################################
 ############Data exploration##########################################
 
 
-# make a plot
+#Plots Persi number of false alerts and sum duration of false alerts
 library(ggplot2)
-?ggplot
 ggplot(persi, aes(x = rep_total, y = Number.False.Alerts)) +
   geom_col (fill = "skyblue") +
 labs(title = "Persi False Alerts Over Time (Repetitions)", x = "Repetition #", y = "# False Alerts")
 
-ggplot(persi, aes(x = rep_total, y = FA1..s.)) +
+ggplot(persi, aes(x = rep_total, y = FAduration)) +
   geom_col (fill = "skyblue") +
-  labs(title = "Persi False Alerts Over Time (Repetitions)", x = "Repetition #", y = "False Alert 1 Duration")
+  labs(title = "Persi False Alerts Over Time (Repetitions)", x = "Repetition #", y = "False Alert Duration")
+
+
+#Plots Madi number of false alerts and sum duration of false alerts
+ggplot(madi, aes(x = rep_total, y = Number.False.Alerts)) +
+  geom_col (fill = "skyblue") +
+  labs(title = "Madi False Alerts Over Time (Repetitions)", x = "Repetition #", y = "# False Alerts")
+
+ggplot(madi, aes(x = rep_total, y = FAduration)) +
+  geom_col (fill = "skyblue") +
+  labs(title = "Madi False Alerts Over Time (Repetitions)", x = "Repetition #", y = "False Alert Duration")
+
+#Number False Alerts per Negative Sample
+ggplot(doi_clean, aes(x = Negative.1, y = Number.False.Alerts)) +
+  geom_col (fill = "skyblue") +
+  labs(title = "Number of False Alerts Per Negative Sample", x = "Sample", y = "Number of False Alerts")
 
 
 
-# Add sum of all false alerts for each repetition
-# Sum across rows to create new column
-# Work in progress, kayla commented out to figure out her stuff below
-#?rowSums()
-#madi$totalduration = rowSums(madi[, c("FA1..s.", "FA2..s.", "FA3..s.", "FA4..s.", "FA5..s.", "FA6..s.", "FA7..s.", "FA8..s.", na.rm = TRUE)
 
 
 # Kayla finding average number of repetitions per session
@@ -92,3 +107,9 @@ reps_by_session <- count(doi_clean, session, name = "reps")
 
 # get summary stats
 summary(reps_by_session)
+
+#Here's another way -RH
+result <- madi %>%
+  group_by(Date) %>%
+  summarize(AverageValue = mean(Rep.Number, na.rm = TRUE))
+mean(result$AverageValue)
